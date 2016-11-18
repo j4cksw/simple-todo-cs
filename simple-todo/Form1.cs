53a3c9bd49eda320c61d2e9b8253f26aee1330b9
Prayoch <prayoch@clockupstudio.com>
2016-11-18 11:50:55 +0000

    Initialized project

---
+++ simple-todo/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace simple_todo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if( textBox1.Text == "")
+            {
+                return;
+            }
+
+            ListViewItem newItem = new ListViewItem(DateTime.Now.ToString());
+            //newItem.SubItems.Add("10/10/2010 10:10");
+            newItem.SubItems.Add(textBox1.Text);
+
+            noteList.Items.Add(newItem);
+
+            textBox1.Text = "";
+        }
+    }
+}
